514a042fd35f381456e53b9d1626c1647dd270f8
長島光太 <se.kokuto@gmail.com>
2018-12-24 22:52:09 +0900

    first

---
+++ Assets/HoloTap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class HoloTap : MonoBehaviour, IInputClickHandler {
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        gameObject.AddComponent<Rigidbody>();
+    }
+
+    void Start ()
+    {
+		
+	}
+
+
+    void Update ()
+    {
+		
+	}
+}
